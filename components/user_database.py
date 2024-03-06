26037f4c826a6d27d587b200d0f52ff64301bb39
Korbin Deary <korbin.deary45@gmail.com>
2024-03-06 11:37:51 -0500

    db comments

--- components/user_database.py
+++ components/user_database.py
@@ -9,12 +9,21 @@ roomsTable = db.table('rooms')
 User = Query()
 Room = Query()
 
+# The following functions are used to interact with the database
+
+# =======================
+# User functions
+# =======================
+
+# Get a user by their username
 def getUser(username):
 	return usersTable.get(User.username == username)
 
+# Get a user by their session token
 def getUserBySessionToken(sessionToken):
 	return usersTable.get(User.session_token == sessionToken)
 
+# Create a new user
 def createUser(username):
 	session_token = str(uuid.uuid1())
 
@@ -28,23 +37,34 @@ def createUser(username):
 
 	return newUser
 
+# =======================
+# Room functions
+# =======================
+
+# Get all rooms
 def getRooms():
 	return roomsTable.search(Room.fragment({}))
 
+# Get a room by its id
 def getRoom(room_id):
 	return roomsTable.get(Room.id == room_id)
 
+# Create a new room
 def createRoom(room_name):
 	newUser = {
 		'id': str(uuid.uuid1()),
 		'name': room_name,
-		'members': [],
 		'room_ids': []
 	}
 	usersTable.insert(newUser)
 
 	return newUser
 
+# =======================
+# Chat functions
+# =======================
+
+# Send a chat message to a room
 def sendChatMessage(room_id, user_session_token, message):
 	foundRoom = getRoom(room_id)
 	author = getUserBySessionToken(user_session_token)
@@ -61,24 +81,22 @@ def createMessageObject(authorId, authorName, body):
 		'date': str(datetime.now())
 	}
 
+# Create the main lobbies if they don't exist
 if len(roomsTable.search(Room.id == 'MAIN')) == 0:
 	roomsTable.insert({
 		'id': 'MAIN',
 		'name': 'Main Lobby',
-		'members': [],
 		'messages': [createMessageObject('ADMIN', 'Admin', 'Welcome to the Main Lobby!')]
 	})
 
 	roomsTable.insert({
 		'id': 'OTHER',
 		'name': 'Other Lobby',
-		'members': [],
 		'messages': [createMessageObject('ADMIN', 'Admin', 'Welcome to the Other Lobby!')]
 	})
 
 	roomsTable.insert({
 		'id': 'ANOTHER',
 		'name': 'Another Lobby',
-		'members': [],
 		'messages': [createMessageObject('ADMIN', 'Admin', 'Welcome to Another Lobby!')]
 	})
