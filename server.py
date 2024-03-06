bc76df12fffa3bf211178e680b1624c5698ca0ae
Korbin Deary <korbin.deary45@gmail.com>
2024-03-05 21:06:22 -0500

    removed stuff for exercise

--- server.py
+++ server.py
@@ -16,82 +16,19 @@ def index():
 def send_static(path):
 	return send_from_directory('static', path)
 
-# Session route
-@app.route('/get_session')
-def getSession():
-	session_token = request.cookies.get('session_token')
-	
-	foundUser = getUserBySessionToken(session_token)
-	return json.dumps({
-		'user': foundUser
-	})
+# Session Route (GET)
 
-# Login Route
-@app.route('/login', methods=['POST'])
-def loginUser():
-	username = request.form['username']
-	user = getUser(username)
-	if user is None:
-		user = createUser(username)
-	print(user)
+# Login Route (POST)
 
-	cResponse = make_response('{"loggedIn": true}');
-	
-	cResponse.set_cookie('session_token', user['session_token'])
-	return cResponse
+# Get All Rooms route (GET)
 
-# Rooms route
-@app.route('/rooms')
-def getRoomsRoute():
-	rooms = getRooms()
+# Get Room by ID route (GET)
 
-	for room in rooms:
-		room['messages'] = room['messages'][-3:]
-
-	return json.dumps({
-		'rooms': rooms
-	})
-
-# Room route
-@app.route('/room/<room_id>')
-def getRoomRoute(room_id):
-	room = getRoom(room_id)
-	return json.dumps({
-		'room': room
-	})
-
+# WebSockets server
 @sock.route('/ws')
 def socketServer(ws):
-	session_token = request.cookies.get('session_token')
-
-	if session_token is None:
-		return 'no session token'
-
-	clients[session_token] = ws
-
 	while True:
 		data = ws.receive()
-		event_name = ''
-		event_data = {}
-
-		try:
-			ws.send('["ping"]')
-		except Exception as e:
-			del clients[session_token]
-			print(e.__class__, flush=True)
-		
-		try:
-			parsed = json.loads(data);
-			event_name = parsed[0]
-			event_data = parsed[1]
-		except Exception as e:
-			print('Failed to parse JSON passed through websocket', e)
-
-		if event_name == 'chat':
-			messageObj = sendChatMessage(event_data['room_id'], session_token, event_data['body'])
-			for pair in clients.items():
-				client = pair[1]
-				client.send(json.dumps(['chat', messageObj]))
 
 if __name__ == "__main__":
 	app.run(debug=True)
